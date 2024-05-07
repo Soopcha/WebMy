c3fe81e400a5270fc1631449bdd9b36a86ee9ce5
Soopcha <lena.malykhina.04@gmail.com>
2024-05-08 00:49:20 +0300

    дополнение сделала

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import Flask, render_template, request
 import csv
 import zipfile
 
+# Инициализация Flask приложения
 app = Flask(__name__)
 
 # Функция загрузки данных из архивированного файла MNP.csv
@@ -20,18 +21,21 @@ def load_mnp_data(filename):
         with zfile.open('MNP.csv', mode='r') as file:
             reader = csv.reader((line.decode('utf-8') for line in file))
             next(reader)  # Пропускаем заголовок
+            # Обработка строк файла и добавление информации в словарь
             for row in reader:
                 number = row[0]
                 ported_to_operator = row[1]
                 mnp_data[number] = ported_to_operator
     return mnp_data
 
-# Загружаем данные из CSV-файла
+# Загружаем данные из CSV-файла Numbers-Plan-9.csv
 def load_number_plan(filename):
     number_plan = {}
+    # Открытие CSV-файла и чтение данных
     with open(filename, mode='r', encoding='utf-8') as file:
         reader = csv.reader(file)
         next(reader)  # Пропускаем заголовок
+        # Обработка строк файла и добавление информации в словарь
         for row in reader:
             number = row[0]
             operator = row[1]
@@ -48,12 +52,14 @@ def index():
     return render_template('index.html')
 
 # Обработка запроса и вывод результата
-# Обработка запроса и вывод результата
 @app.route('/search', methods=['POST'])
 def search():
+    # Загрузка данных о плане нумерации и данных о переносе номеров
     number_plan = load_number_plan('Numbers-Plan-9.csv')
     mnp_data = load_mnp_data('MNP.zip')  # Загружаем данные из архивированного файла MNP.zip
+    # Получение номера из формы
     number = request.form['number']
+    # Получение информации о номере из плана нумерации
     info = number_plan.get(number[:3], {'operator': 'Оператор не найден', 'region': ['Регион не найден']})
     operator = info['operator']
     regions = info['region']
@@ -65,5 +71,6 @@ def search():
     else:
         return render_template('result.html', operator=operator, regions=regions,
                                ported_to_operator="Номер не был перенесен")
+# Запуск Flask приложения
 if __name__ == '__main__':
     app.run(debug=True)
